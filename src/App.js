8823fc2f95d9644e1433ef973def74b18751514c
Tameem <tameem_r@hotmail.com>
2021-04-06 14:27:45 +0300

    state: song, currnetSong, isPlaying/ create components player and song/  create util (data)/  playSongHandler/ styling by SASS

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+// import style
+import "../src/style/app.scss";
+// import components
+import Song from "./components/Song";
+import Player from "./components/Player";
+// import Util
+import data from "./util";
+
+function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+    </div>
+  );
+}
+
+export default App;
